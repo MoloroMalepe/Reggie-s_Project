c5bf93679f1c954854dbb4a76e525e5f25aef13a
Moloro Malepe <moloromalepe17@gmail.com>
2021-06-23 16:43:18 +0200

    My files are being saved.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reggie login Form</title>
+    <link rel="stylesheet" type="text/css" href="./styles.css">
+   
+</head>
+<body>
+    <div class="Login-form">
+    <h1>Reggie's Login Form</h1>
+    <form action="#" method="post">
+    
+    
+    <div>
+
+      <input type="text" name="user" placeholder="Username">
+
+    </div>
+    <br>
+<div>
+
+  <input type="password" name="password" placeholder="Password">
+
+  
+</div>
+    
+    <br>
+    <button type="Login">Login</button>
+    </form>
+    </div>
+
+    <div>
+        <label>
+            Forgot password?
+        </label> 
+    </div>
+ method </body>
+</html>
